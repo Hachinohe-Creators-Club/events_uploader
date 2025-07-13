cea38e7b674d607c06f63a61946f21d857b471a3
Yuta Sukekawa <sukekawa@thymefield.jp>
2025-07-13 18:22:22 +0900

    add: Add log

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
+import logging
 from fastapi import FastAPI, Request, HTTPException
 from pydantic import BaseModel
 from fastapi.responses import PlainTextResponse
+
+# ロガーの基本設定
+logging.basicConfig(
+    level=logging.INFO,  # ログレベル
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+
+# アプリケーション用のロガーを取得
+logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
@@ -15,6 +25,7 @@ async def slack_url_verification(request: Request):
 
     # Slack の URL 検証リクエストかどうかを確認
     if body.get("type") == "url_verification":
+        logging.info('Slack URL verification request received.')
         return PlainTextResponse(content=body.get("challenge"), status_code=200)
     
     # その他のリクエストには 200 OK を返す（必要に応じて他の処理をここで実装）
